79de62a96be6c11418d21e9f12daa25d45f573bd
Lukas Szimtenings <25265369+lukszi@users.noreply.github.com>
2023-10-08 12:16:22 +0200

    Fix improper deletion of playlist_select file

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,3 @@
 from app.routes.authorization import router as authorization_router
-from app.routes.playlist_select import router as playlist_select_router
 from app.routes.playlist_overview import router as playlist_overview_router
 from app.routes.playlist_optimize import router as playlist_optimize_router
--- app/run_server.py
+++ app/run_server.py
@@ -3,11 +3,10 @@ import uvicorn
 from fastapi.responses import RedirectResponse
 
 from app.exceptions import NotLoggedInException, not_logged_in_exception_handler
-from app.routes import playlist_select_router, authorization_router, playlist_overview_router, playlist_optimize_router
+from app.routes import authorization_router, playlist_overview_router, playlist_optimize_router
 app = FastAPI()
 
 # utilize the routes from the other files
-app.include_router(playlist_select_router)
 app.include_router(authorization_router)
 app.include_router(playlist_overview_router)
 app.include_router(playlist_optimize_router)
